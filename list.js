b65ab4f1b319c632194f8e621ff4bfd95f1dc68b
Ryan Walsh <ryan@thehatrack.net>
2016-02-22 14:32:06 -0600

    initial commit

---
+++ list.js
@@ -0,0 +1,33 @@
+/* Make the following code work:
+sum(2, 3) // 5
+sum(2)(3) // 5
+*/
+
+///////////////////////
+
+/*
+Add a sum method to the array prototype
+var arr = [1,2,3,4];
+arr.sum();  //returns 10
+
+var arr = [10, 28, 14, 33];
+arr.sum(); //returns 85
+*/
+
+///////////////////////
+
+/*
+Write a function that takes a number parameter 
+and returns the smallest palindrome number higher than the parameter
+
+nextPal(11) == 22
+
+nextPal(188) == 191
+
+nextPal(191) == 202
+
+nextPal(2541) == 2552
+*/
+
+////////////////////////
+
