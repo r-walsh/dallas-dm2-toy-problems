1b4164b45164c71a4b63c60a36e1fb6010d823a7
Ryan Walsh <ryan@thehatrack.net>
2016-02-24 08:54:37 -0600

    Added problem

--- list.js
+++ list.js
@@ -43,3 +43,15 @@ No trailing spaces!
 */
 
 /////////////////////////
+
+/*
+write a function that will console log the numbers 1 through 5, 
+with delays between each log equaling the logged number.
+delayLog() // pauses 1 second, then logs 1
+// pauses 2 seconds after the previous log, then logs 2
+// pauses 3 seconds after the previous log, then logs 3
+// pauses 4 seconds after the previous log, then logs 4
+// pauses 5 seconds after the previous log, then logs 5
+*/
+
+/////////////////////////
