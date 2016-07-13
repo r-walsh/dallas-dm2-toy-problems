4134501ec0d3fd1dc5b6b81a8238d3d76b3e65a0
Ryan Walsh <ryan@thehatrack.net>
2016-07-13 06:26:34 -0500

    Update list.js

--- list.js
+++ list.js
@@ -648,3 +648,18 @@ zebulansNightmare('main') == 'main'
   obj=[1,2,3,2,1]      return true
   obj=[11,12,13,12,11] return true   (left element = right element)
   obj=[11,12,21,11]    return false  (not verify them as a string)
+  
+///////////////////////////////
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
+*/
+
+solution( 10 ); // 23
+solution( 45 ); // 450
+solution( 82 ); // 1589
+solution( 11 ); // 33
+
+///////////////////////////////
